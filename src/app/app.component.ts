7fb70dde0c890218594650cf08273d24ae7990f5
dhekra <dhekra.benhassine.1992@gmail.com>
2023-06-17 15:05:19 +0100

    show/hide SideBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GestionStockClient';
+  ShowHideSideBar:boolean = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import { ProduitMockService} from './produit/produit.mock.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
+    NavbarComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule
